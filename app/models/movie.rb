59962efc30375cb00219fda5ecc840ae502db76b
aidai beishekeeva <ab5248@columbia.edu>
2021-10-04 18:35:43 +0000

    m

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,13 @@
 class Movie < ActiveRecord::Base
   def self.all_ratings 
     ['G','PG','PG-13','R']
-  end 
+  end
+
+  def with_ratings(ratings)
+   if not ratings.nil? 
+     Movie.where("rating IN (?)", ratings)
+   else
+     Movie.all
+   end 
+  end  
 end
